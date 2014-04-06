b2b4f575ea4cbf7f1af5045b78dd98817a50827a
duhoobo <alecdu@gmail.com>
2014-04-06 17:08:53 +0800

    test case for `misc.dot` and `misc.eol` added

---
+++ tests/misc.lua
@@ -0,0 +1,13 @@
+package.path = "/home/duhoobo/prj/amateur/lua-resty-smtp/lib/?.lua;" .. package.path
+local mime = require("mime")
+local misc = require("resty.misc")
+
+
+print(mime.dot(2, ".\r\nStuffing the message.\r\n.\r\n."))
+print "---"
+print(misc.dot(2, ".\r\nStuffing the message.\r\n.\r\n."))
+
+dos = "abcd\nefg"
+
+print(#mime.eol(0, dos, "\r\n"))
+print(#misc.eol(0, dos, "\r\n"))
