03f0262ddbaf49cb689271d4a2050799921082ce
duhoobo <alecdu@gmail.com>
2014-04-09 12:09:04 +0800

    Luasocket sendmail client for comparison test

---
+++ tests/luasocket-sendmail.lua
@@ -0,0 +1,49 @@
+
+local mime = require("mime")
+local smtp = require("socket.smtp")
+local socket = require("socket")
+
+
+
+function sendmail()
+    from= "<wireless.alarm@baofeng.com>"
+
+    rcpt= {
+        "<15811046760@139.com>"
+    }
+
+    mesgt = {
+        headers= {
+            subject = "My first message",
+            ["content-transfer-encoding"] = "BASE64",
+            ["content-type"] = "text/plain; charset='utf-8'",
+        },
+
+        body= ltn12.source.chain(
+            ltn12.source.string("中文内容"),
+            mime.encode("base64"))
+    }
+
+    r, e = smtp.send {
+        from= from,
+        rcpt= rcpt,
+        source= smtp.message(mesgt),
+        server= "mail.baofeng.com",
+        domain= "wireless-alarm",
+        user= "wireless.alarm@baofeng.com",
+        password= "qwe123",
+        create= socket.tcp,
+    }
+
+    print(r)
+    print(e)
+end
+
+
+function main()
+    sendmail()
+end
+
+
+main()
+
